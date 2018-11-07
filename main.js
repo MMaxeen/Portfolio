3e9d57081f52340d84ee2136129f91b8ba816012
Maxime Motte <maxeen17@gmail.com>
2018-11-07 15:51:03 +0100

    gyro ????

--- main.js
+++ main.js
@@ -6,8 +6,35 @@
 //     //rough wiggle effect before bursting forth into a rectangle...
 // 		.to("#polygon", 0.1, {rotation:-5, transformOrigin:"50% 50%", delay:0.5})
 // 		.to("#polygon", 0.5, {rotation:10, scale:1.2, ease:RoughEase.ease.config({strength:2, template:Back.easeInOut}), repeat:1, yoyo:true})
+var jardin = document.querySelector('.canvas'),
+    balle = document.querySelector('.cursor'),
+    maxX = jardin.clientWidth  - balle.clientWidth,
+    maxY = jardin.clientHeight - balle.clientHeight;
 
+function handleOrientation(event) {
+  var x = event.beta,  // En degré sur l'interval [-180,180].
+      y = event.gamma; // En degré sur l'interval [-90,90].
 
+  resultat.innerHTML  = "beta : " + x + "<br />";
+  resultat.innerHTML += "gamma: " + y + "<br />";
+
+  // Parce-que l'on ne veut pas avoir l'appareil à l'envers.
+  // On restreint les valeurs de x à l'intervalle [-90,90].
+  if (x >  90) { x =  90};
+  if (x < -90) { x = -90};
+  // Pour rendre le calcul plus simple.
+  // On délimite l'intervalle de x et y sur [0, 180].
+  x += 90;
+  y += 90;
+
+  // 10 est la moitié de la taille de la balle.
+  // Cela centre le point de positionnement au centre de la balle.
+
+  balle.style.top  = (maxX * x / 180 - 10) + "px";
+  balle.style.left = (maxY * y / 180 - 10) + "px";
+}
+
+window.addEventListener('deviceorientation', handleOrientation);
 var root = document.querySelector('html');
 
 // Real cursor element
@@ -139,7 +166,7 @@ function init() {
     document.body.appendChild( ( stats = new Stats() ).domElement );
   }
   
-  container.appendChild( canvas );
+  document.querySelector( '.canvas' );
 }
 
 function step() {
